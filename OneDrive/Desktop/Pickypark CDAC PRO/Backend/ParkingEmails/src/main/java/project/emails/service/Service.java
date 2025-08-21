32ef875fee56fdda4d6ef5ef115bdc0382d2ef2a
shreyashtitare999 <shreyashtitare999@gmail.com>
2025-08-21 07:52:55 +0530

    Delete OneDrive/Desktop/Pickypark CDAC PRO directory

--- OneDrive/Desktop/Pickypark CDAC PRO/Backend/ParkingEmails/src/main/java/project/emails/service/Service.java
+++
@@ -1,8 +0,0 @@
-// project/emails/service/EmailService.java
-package project.emails.service;
-
-import project.emails.entity.Email;
-
-public interface Service {
-    String sendSimpleMail(Email details);
-}
